2822d55ecfd747577d4167da32932078094497f4
Mukund Pandit <mukund.pandit@virginmedia.com>
2021-10-12 20:36:24 +0100

    Just testing. All files are blank

---
+++ js-file.js
@@ -0,0 +1,63 @@
+/*
+const container = document.querySelector('#container');
+
+const redPara = document.createElement('p');
+
+const blueH3 = document.createElement('h3');
+
+const divContainer = document.createElement('div');
+
+const redParaTwo = document.createElement('p');
+
+const blueH3Two = document.createElement('h3');
+
+*/
+
+/*
+const MyCarFactory = function (manufact, make, model, beauty) {
+let internalMake = make
+let internalModel = model
+let internalManufact = manufact
+let internalBeauty = beauty
+
+let myPrivateMethod1 = () => {
+  if (internalBeauty === 1) {
+    console.log(`This car is a ${internalModel},
+    whose brand is ${internalMake},
+    whose parent company is ${internalManufact}.
+    It is exquisitely beautiful. `)
+                            }
+
+  if (internalBeauty === 2){
+    console.log(`This car is a ${internalModel},
+    whose brand is ${internalMake},
+    whose parent company is ${internalManufact}.
+    It is acceptably good looking.`)
+                           }
+
+  if (internalBeauty === 3){
+    console.log(`This car is a ${internalModel},
+    whose brand is ${internalMake},
+    whose parent company is ${internalManufact}.
+    It is so ugly the designer should have been pilloried.`)
+                           }
+
+
+                              }  // end fn
+
+let myPublicMethod1 = () => {
+  myPrivateMethod1()
+                            }  // end fn
+
+return { myPublicMethod1 }
+
+                                                        } // end factory
+
+const allegro = MyCarFactory("British Leyland", "Austin", "Allegro", 3)
+const eType = MyCarFactory("British Leyland", "Jaguar", "E type ", 1)
+const Rover3point5 = MyCarFactory("British Leyland", "Rover", "Rover 3.5 litre", 2)
+
+// allegro
+// eType
+// Rover3point5
+*/
